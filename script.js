97be3d5329df9faf38699a59636d952ed7d0507f
Jean <jeansvf@gmail.com>
2022-12-22 08:42:48 -0300

    added the contextmenu

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
-var input = document.querySelector(".chat-input");
-const list = document.querySelector(".messages-list");
-var messageElement = document.querySelector(".message-content");
+const input = document.querySelector(".chat-input");
+const messagesList = document.querySelector(".messages-list");
+const messageElement = document.querySelector(".message-content");
+var messageListItem;
+var contextMenuDiv;
+var contextMenuDivDeleteButton;
 var messageId = 0;
+var selectedMessage;
 
 const createMessage = () => {
-    let messageLi = document.createElement("li");
-    messageLi.classList.add("message-content", messageId + 1);
-    messageLi.innerText = input.value;
-    list.appendChild(messageLi);
+    messageListItem = document.createElement("li");
+    messageListItem.classList.add("message-content", messageId + 1);
+    messageListItem.innerText = input.value;
+    messagesList.appendChild(messageListItem);
     input.value = "";
     messageId++;
-    messageLi.addEventListener("contextmenu", () => {
-        messageLi.remove();
+    messageListItem.addEventListener("contextmenu", (e) => {
+    selectedMessage = e.target;
+    createContextMenu();
     })
 }
 
-document.addEventListener( "keydown", () => {
-    if (event.key === "Enter" && input.value !== "") {
+const createContextMenu = () => {
+    if (contextMenuDiv !== undefined){
+        contextMenuDiv.remove();
+    }
+
+    contextMenuDiv = document.createElement("div");
+    contextMenuDiv.classList.add("context-menu-div");
+    contextMenuDiv.style.left = event.clientX + "px";
+    contextMenuDiv.style.top = event.clientY + "px";
+    messagesList.appendChild(contextMenuDiv);
+
+    contextMenuDivDeleteButton = document.createElement("div");
+    contextMenuDivDeleteButton.classList.add("context-menu-div-delete-button");
+    contextMenuDivDeleteButton.innerText = "Delete"
+    contextMenuDiv.appendChild(contextMenuDivDeleteButton);
+
+    contextMenuDivDeleteButton.addEventListener("click", () => {
+        console.log(selectedMessage);
+        selectedMessage.remove();
+    })
+}
+
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && input.value !== "") {
         createMessage();
     }
 })
 
-document.addEventListener("contextmenu", () => {
-    event.preventDefault();
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+})
+
+document.addEventListener("click", () => {
+    contextMenuDiv.remove();
 })
